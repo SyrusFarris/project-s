8ddcf7cae5a8215480b85cdc86965f992a61fe20
Syrus <syrusfarris@gmail.com>
2023-05-26 16:05:08 -0500

    resetting various files and programs on local machine

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <Header>
+        <div className="container_header">
+            <h1>Project S</h1>
+                <h3>The world's online car meet</h3>
+        </div>
+    </Header>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+
 
 
 ReactDOM.render(<App/>, document.querySelector("#root"));
